242c245f81a5f0f6eca862121517feb483e86840
ergonmehmeti <mehmeti.ergon@gmail.com>
2024-12-05 14:46:51 +0100

    puma configuration

--- config/puma.rb
+++ config/puma.rb
@@ -20,15 +20,26 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
+# Env
+
+environment ENV.fetch("RAILS_ENV") { "production" }
+
+workers Integer(ENV.fetch("WEB_CONCURRENCY", 6))
+
+preload_app!
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
--- db/migrate/20241102164652_drop_lendet_gjyqesores.rb
+++
@@ -1,5 +0,0 @@
-#class DropLendetGjyqesores < ActiveRecord::Migration[7.2]
-#def change
-# drop_table :lendet_gjyqesores
-# end
-#end
